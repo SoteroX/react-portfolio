611955fe1a657e2120460f9eb86c9a6034c84c60
SoteroX <soterox13@gmail.com>
2018-08-15 08:41:53 -0500

    fix profile pic path

--- src/layout/home/project/Project.jsx
+++ src/layout/home/project/Project.jsx
@@ -6,19 +6,19 @@ const Project = () => (
       <h1 className="project-header-text">Projects</h1>
       <div className="gallery-container">
         <div>
-          <img src="../../../asset/img/coming-soon.png" alt="" />
+          <img src="../../asset/img/coming-soon.png" alt="" />
         </div>
         <div>
-          <img src="../../../asset/img/coming-soon.png" alt="" />
+          <img src="../../asset/img/coming-soon.png" alt="" />
         </div>
         <div className="vertical">
-          <img src="../../../asset/img/coming-soon.png" alt="" />
+          <img src="../../asset/img/coming-soon.png" alt="" />
         </div>
         <div className="big">
-          <img src="../../../asset/img/coming-soon.png" alt="" />
+          <img src="../../asset/img/coming-soon.png" alt="" />
         </div>
         <div>
-          <img src="../../../asset/img/coming-soon.png" alt="" />
+          <img src="../../asset/img/coming-soon.png" alt="" />
         </div>
       </div>
     </div>
